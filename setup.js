5a44c133a01c80e721e7c2ef1f3d23d9c1530448
Jinny You <baram991103@gmail.com>
2022-12-09 16:48:56 +0900

    :sparkles: Add ads

--- setup.js
+++ setup.js
@@ -5,4 +5,25 @@ if (window.location.hash) {
   target.src = `assets/${pathname}/default_100_percent/100-offline-sprite.png`;
   const target2 = document.querySelector('#offline-resources-2x');
   target2.src = `assets/${pathname}/default_200_percent/200-offline-sprite.png`;
+
+  const ad = document.querySelector('.ad > img');
+  ad.src = `assets/banners/${pathname}.png`;
+}
+
+function onAd() {
+  const pathname = window.location.hash.slice(1);
+  let link = 'https://onelink.to/2n7b9c';
+
+  switch (pathname) {
+    case 'dongdong':
+      link = 'https://second-team.com';
+      break;
+    case 'ghost':
+      link = 'https://www.instagram.com/fermentedghost/';
+      break;
+    default:
+      break;
+  }
+
+  window.open(link);
 }
